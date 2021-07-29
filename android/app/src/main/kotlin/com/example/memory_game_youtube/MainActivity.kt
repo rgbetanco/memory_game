daf9df668d78b4b224bfe9bd7c3fc4560dc1518a
Ronald Garcia <rgbetanco@hotmail.com>
2021-07-29 15:42:26 +0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/memory_game_youtube/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.memory_game_youtube
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'dart:io';
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(scaffoldBackgroundColor: Colors.black),
+      home: MemoryGame()));
+}
+
+class MemoryGame extends StatefulWidget {
+  const MemoryGame({Key? key}) : super(key: key);
+
+  @override
+  _MemoryGameState createState() => _MemoryGameState();
+}
+
+class _MemoryGameState extends State<MemoryGame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  child: Padding(
+                    padding: EdgeInsets.all(100.0),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(26.0),
+                child: Container(
+                      height: 130,
+                      width: 130,
+                      child: FloatingActionButton(
+                        backgroundColor: Colors.blue,
+                        onPressed: () {},
+                        elevation: 1,
+                      ),
+                    ),),
+              ],
+            ),
+            Row(
+              children: [],
+            ),
+            Row(
+              children: [],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
